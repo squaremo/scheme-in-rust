44891c5f855efd4866290a3aef2bd0db2e251df3
Michael Bridgen <mikeb@squaremobius.net>
2020-12-13 10:16:23 +0000

    Make the prommpt a const

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 // `extern` is used in the how-to; more modern is to just `use`.
 use rustyline;
 
+const PROMPT: &str = "; ";
+
 fn main() {
     // the `mut` means I can _and must_ reassign the value
     let mut done = false;
     let mut reader = rustyline::Editor::<()>::new();
     while !done {
-        match reader.readline(">> ") {
+        match reader.readline(PROMPT) {
             Ok(line) =>
                 if line == "(exit)" {
                     done = true;
