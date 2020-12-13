3de65f2c2bee35cadbd9da83090006f66888dca1
Michael Bridgen <mikeb@squaremobius.net>
2020-12-13 10:03:38 +0000

    Use rustyline crate for R(E)PL
    Per the how-to.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// `extern` is used in the how-to; more modern is to just `use`.
+use rustyline;
+
 fn main() {
-    println!("Hello, world!");
+    // the `mut` means I can _and must_ reassign the value
+    let mut done = false;
+    let mut reader = rustyline::Editor::<()>::new();
+    while !done {
+        match reader.readline(">> ") {
+            Ok(line) =>
+                if line == "(exit)" {
+                    done = true;
+                } else {
+                    println!("{}",line); // println accepts a format string?
+                },
+            Err(e) => println!("Couldn't readline: {}", e),
+        }
+    }
 }
