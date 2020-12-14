b8082609cfcfa33d64129cb6c6f154bcf7a81b8e
Michael Bridgen <mikeb@squaremobius.net>
2020-12-14 16:17:11 +0000

    Use iterators for primitives
    This adds another primitive, `*`, and rewrites `+` to use
    iterators. There's a .sum() which seems to account for taking Results
    (short-circuiting?), but no `.product()`; `.try_fold()` looks like the
    nearest useful thing.

--- src/prims.rs
+++ src/prims.rs
@@ -2,16 +2,26 @@ use crate::value::{ValueRef,Value,make_prim};
 use crate::env::Env;
 
 pub fn plus(args: Vec<ValueRef>) -> Result<Value, String> {
-    let mut result: i64 = 0;
-    for arg in args {
+    let result: Result<i64, String> = args.into_iter().map(| arg | {
         match *arg {
-            Value::Int(i) => result += i,
-            _ => return Err(String::from("attempt to add non-number"))
-        };
-    }
-    Ok(Value::Int(result))
+            Value::Int(i) => Ok(i),
+            _ => Err(String::from("attempt to add non-number"))
+        }
+    }).sum();
+    result.map(|i| Value::Int(i))
+}
+
+pub fn mult(args: Vec<ValueRef>) -> Result<Value, String> {
+    let result: Result<i64, String> = args.into_iter().try_fold(1i64, | acc, arg | {
+        match *arg {
+            Value::Int(i) => Ok(i * acc),
+            _ => Err(String::from("attempt to multiply non-number"))
+        }
+    });
+    result.map(Value::Int)
 }
 
 pub fn into_env(env: &mut Env) {
     env.set_env("+", make_prim(plus));
+    env.set_env("*", make_prim(mult));
 }
